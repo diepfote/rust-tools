6a874919e9e294978c7ca6a463674ac3e5d0d5a0
Florian Sorko <florian.begusch@gmail.com>
2025-09-25 17:18:20 +0000

    Limit to files and print modified/created

--- filestile/src/main.rs
+++ filestile/src/main.rs
@@ -1,5 +1,6 @@
 use std::env;
 use std::fs;
+use chrono::{DateTime, Utc};
 
 fn main() {
 
@@ -9,10 +10,30 @@ fn main() {
     match fs::read_dir(path) {
         Ok(dir) => {
             for entry in dir {
-                match entry {
-                    Ok(entry) => println!("{}", entry.file_name().to_string_lossy()),
-                    Err(err) => println!("Error: {}", err),
-                }
+                if let Ok(entry) = entry {
+                    if let Ok(metadata) = entry.metadata() {
+                        if metadata.is_dir() {
+                            continue;
+                        }
+                        let modified_nsec = metadata.modified();
+                        let created_nsec = metadata.created();
+
+                        if let Ok(modified_nsec) = modified_nsec {
+                            let modified: DateTime<Utc> = DateTime::<Utc>::from(modified_nsec);
+                            println!("{}: modified @{}", entry.file_name().to_string_lossy(), modified);
+                        }
+
+                        if let Ok(created_nsec) = created_nsec {
+                            let created: DateTime<Utc> = DateTime::<Utc>::from(created_nsec);
+                            println!("{}: created @{}", entry.file_name().to_string_lossy(), created);
+                        }
+
+                        // println!("{}", entry.file_name().to_string_lossy());
+                        // println!("{:?}: {:?}", entry.path(), metadata.permissions());
+                        // println!("{}: {:?}", entry.file_name().to_string_lossy(), metadata.permissions());
+                    }
+
+                 }
             }
         }
         Err(err) => println!("Error reading directory: {}", err),
