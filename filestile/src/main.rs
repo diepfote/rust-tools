0d3fded536616e46aabebd0fad835357298296a0
Florian Sorko <florian.begusch@gmail.com>
2025-10-01 07:34:02 +0000

    Use lexopt for argument parsing

--- filestile/src/main.rs
+++ filestile/src/main.rs
@@ -1,11 +1,52 @@
-use std::env;
 use std::fs;
 use chrono::{DateTime, Utc};
 
-fn main() {
+struct Args {
+    path: String,
+    pattern: String,
+}
 
+fn parse_args() -> Result<Args, lexopt::Error> {
+    use lexopt::prelude::*;
 
-    let path = env::args().nth(1).expect("No path given");
+    let mut path = None;
+    // @TODO: allow multiple patterns
+    let mut pattern = None;
+    // @TODO: allow multiple patterns
+    // let mut literal = None;
+    let mut parser = lexopt::Parser::from_env();
+    while let Some(arg) = parser.next()? {
+        match arg {
+            // Short('f') => {
+            //     literal = parser.value()?.string()?;
+            // }
+
+            Short('e') | Long("pattern") => {
+                pattern = Some(parser.value()?.string()?);
+            }
+
+            Value(val) if path.is_none() => {
+                path = Some(val.string()?);
+            }
+            Long("help") => {
+                println!("Usage: filestile --pattern PATTERN PATH");
+                std::process::exit(0);
+            }
+            _ => return Err(arg.unexpected()),
+        }
+    }
+
+    Ok(Args {
+        pattern: pattern.ok_or("missing argument --pattern")?,
+        path: path.ok_or("missing argument PATH")?,
+    })
+}
+
+fn main() -> Result<(), lexopt::Error> {
+
+    let args = parse_args()?;
+    let path = args.path;
+    // let pattern = args.pattern;
 
     match fs::read_dir(path) {
         Ok(dir) => {
@@ -37,5 +78,7 @@ fn main() {
             }
         }
         Err(err) => println!("Error reading directory: {}", err),
+
     }
+       Ok(())
 }
